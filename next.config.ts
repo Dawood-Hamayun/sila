df0074cd17f81c3929a4192352dbc25a9af38e19
Dawood Hamayun <dawoodhamayun2015@gmail.com>
2025-05-24 05:55:15 +0500

    check

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.ts
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors during `next build`
+  },
 };
 
 export default nextConfig;
